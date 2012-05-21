8693404e4e39f52b2088275839576140cc15d250
Antonio <Antonio@ant-portatil7>
2012-05-21 20:35:23 +0200

    Metodos para transacciones en JDBC hechos. Transaccion en añadirBeneficiario probada. Cambio los tipos de CodigoPostal y telefonos de int a String en Modelo y en Base de datos a Varchar

--- src/JDBC/DriverJDBC.java
+++ src/JDBC/DriverJDBC.java
@@ -1,140 +1,182 @@
+
 /**
- ** NOMBRE CLASE: 
+ ** NOMBRE CLASE:
  **	  DriverJDBC.java
  **
  ** DESCRIPCION:
  **       Driver de comunicación con JDBC
- **       
+ **
  **
  ** DESARROLLADO POR:
  *        Francisco José Beltrán Rodriguez (FBR)
- *	   
- **        
+ *
+ **
  ** SUPERVISADO POR:
- **        Adolfo Arcoya Nieto (AAN)  
+ **        Adolfo Arcoya Nieto (AAN)
+ *		   Antonio Rodríguez Segura (ARS)
  **
  ** HISTORIA:
  ** 	000 - Mar 24, 2012 - FBR - Creacion
  **     001 - Mar 25, 2012 - FBR - Implementación de los métodos
  *      002 - Mar 26, 2012 - FBR - Modificación de diversos métodos
  **     003 - Mar 26, 2012 - FBR - Modificación del constructor de la clase, getInstance ahora es public
- *      
+ *		004 - May 21, 2012 - ARS - Añadidos métodos para transacciones
  **
  ** NOTAS:
- **   
+ **
  **
  */
 package JDBC;
 
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- *
- * @author Jobero
- */
 public class DriverJDBC {
-    
-    
-    private Connection conex;
+    private Connection conexion;
     private String hostBD;
     private static DriverJDBC instancia;
     private String nombreBD;
     private String password;
     private String usuarioBD;
     private Statement statement;
-  
-    
-    
-    
-    //Falta mirar bien los close!!
-    
+
+	// Variables para restaurar tras transacciones
+	private boolean oldEstadoAutoCommit;
+	private int oldEstadoTransaccionIsolation;
+
     private DriverJDBC(){
-        
+
     }
-    
+
+    public static DriverJDBC getInstance(){
+		if(instancia == null) instancia = new DriverJDBC();
+		return instancia;
+    }
+
+	public Connection getConexion(){
+		return conexion;
+	}
+
     //Antes de conectar se debera haber configurado
     public boolean conectar(){
-        
-   
         //Siempre usaremos mysql
        try {
             Class.forName("com.mysql.jdbc.Driver");
-            conex = DriverManager.getConnection("jdbc:mysql://"+hostBD+"/"+nombreBD , usuarioBD, password);
-            
-            statement = conex.createStatement();
+            conexion = DriverManager.getConnection("jdbc:mysql://"+hostBD+"/"+nombreBD , usuarioBD, password);
+            statement = conexion.createStatement();
         } catch (SQLException ex) {
-            System.out.println(ex.toString());
+            System.err.println(ex.toString());
             return false;
         } catch (ClassNotFoundException ex1){
-            System.out.println(ex1.toString());
+            System.err.println(ex1.toString());
             return false;
         }
-       
+
         return true;
     }
-    
+
     public boolean desconectar(){
         try {
-            conex.close();
+            conexion.close();
         } catch (SQLException ex) {
+            System.err.println(ex.toString());
             return false;
         }
-        
+
         return true;
     }
-    
+
     public void configurar(String hostBD, String nombreBD, String password, String usuarioBD){
-        
         this.hostBD=hostBD;
         this.nombreBD=nombreBD;
         this.password=password;
         this.usuarioBD=usuarioBD;
-        
     }
-    
-    
-    public boolean actualizar(String sentencia) throws SQLException{
-        
-        /*
-        tabla = statement.executeQuery(sentencia_busqueda);
-	//Para que fuera mas correcto deberíamos comprobar si rs.next devuelve algo (a lo mejor nadie se llama Juan)
-	if( tabla.next() == false)
-            return false;
-	*/
-        
-	//statement.executeUpdate("UPDATE socio SET telefono='111' WHERE id="+id);
-        statement.executeUpdate(sentencia);
-        
-        return true;
-    }
-    
-    public boolean eliminar(String sentencia) throws SQLException{
-        
-        boolean exito= actualizar(sentencia);
-        
-        return exito;
-    }
-    
-    public static DriverJDBC getInstance(){
-        
-        if(instancia == null)
-		instancia = new DriverJDBC();
-	return instancia;
-    }
-    
+
     public boolean insertar(String sentencia) throws SQLException{
-         statement.executeUpdate(sentencia);
-         
+		try{
+			statement.executeUpdate(sentencia);
+		}
+		catch(SQLException ex){
+			throw ex;
+		}
+		return true;
+    }
+
+	public boolean actualizar(String sentencia) throws SQLException{
+        statement.executeUpdate(sentencia);
         return true;
     }
-    
+
     public ResultSet seleccionar (String cadena) throws SQLException{
-        
         return statement.executeQuery(cadena);
     }
-    
-    
+
+    public boolean eliminar(String sentencia) throws SQLException{
+        boolean exito= actualizar(sentencia);
+        return exito;
+    }
+
+	// Métodos para transacciones
+
+	/**
+	 * Indica que empieza una transacción, que acabará con el
+	 * método finTranssacion(), y que no actualizará la base
+	 * de datos hasta hacer commit.
+	 * @throws SQLException errores en la base de datos
+	 */
+	public void inicioTransaccion() throws SQLException {
+		try {
+//			this.conectar();
+			this.oldEstadoTransaccionIsolation = this.conexion.getTransactionIsolation();
+			conexion.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+			this.oldEstadoAutoCommit = this.conexion.getAutoCommit();
+			this.conexion.setAutoCommit(false);
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+
+	/**
+	 * Actualización de la base de datos con las consultas hechas
+	 * @throws SQLException errores en la base de datos
+	 */
+	public void commit() throws SQLException {
+		try {
+			this.conexion.commit();
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+
+	/**
+	 * Deshace las consultas que no se han cometido en la
+	 * base de datos con un comit()
+	 * @throws SQLException errores en la base de datos
+	 */
+	public void rollback() throws SQLException {
+		try {
+			this.conexion.rollback();
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+
+	/**
+	 * Finaliza una trasaccion, cerrando la conexión con la base
+	 * de datos, y restaurando el estado anterior para AutoCommit
+	 * y para TransactionIsolation
+	 * @throws SQLException errores en la base de datos
+	 */
+	public void finTransaccion() throws SQLException {
+		try {
+			// Restauramos estado de autoComit y de Isolation
+			this.conexion.setAutoCommit(this.oldEstadoAutoCommit);
+			this.conexion.setTransactionIsolation(oldEstadoTransaccionIsolation);
+//			this.desconectar();
+		} catch (SQLException e) {
+			throw e;
+		}
+	}
+
 }
 
