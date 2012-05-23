a7f07060b9679a29223cfb8fc7402135d9829503
Raphael Colleau <raphael.colleau@gmail.com>
2012-05-23 16:54:21 +0200

    Adicion metodos ControladorAyuda

--- src/Controladores/Voluntario/ControladorAyuda.java
+++ src/Controladores/Voluntario/ControladorAyuda.java
@@ -1,7 +1,14 @@
 
 package Controladores.Voluntario;
 
+import JDBC.AyudaJDBC;
+import JDBC.MovimientoJDBC;
+import Modelo.Ayuda;
+import Modelo.TipoAyuda;
 import Vistas.Paneles.Voluntario.PanelVoluntarioAyudas;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  ** NOMBRE CLASE:
@@ -20,24 +27,27 @@ import Vistas.Paneles.Voluntario.PanelVoluntarioAyudas;
  **
  ** HISTORIA:
  ** 	000 - 16 mai 2012 - RC - Creacion
+ ** 	001 - 23 mai 2012 - RC - Adicion metodos relacionados con modelo
  **
  ** NOTAS:
  **
  **
  */
 public class ControladorAyuda {
-    /** PATRON DE DISEÑO SINGLETON */
 
+    /**
+     * PATRON DE DISEÑO SINGLETON
+     */
     private static ControladorAyuda instancia = null;
 
-    public static ControladorAyuda getInstance(PanelVoluntarioAyudas pvista){
+    public static ControladorAyuda getInstance(PanelVoluntarioAyudas pvista) {
 
-            if(instancia == null)
-                    instancia = new ControladorAyuda(pvista);
-            return instancia;
-
+        if (instancia == null) {
+            instancia = new ControladorAyuda(pvista);
+        }
+        return instancia;
     }
-
+    
     private PanelVoluntarioAyudas vista;
 
     public ControladorAyuda(PanelVoluntarioAyudas vista) {
@@ -45,7 +55,73 @@ public class ControladorAyuda {
     }
 
     // TODO Metodos JDBC
+    private boolean registrarAyuda(Ayuda ayuda) {
+        boolean exito;
+        
+            // TODO test ayuda.getBeneficiarioDeAyuda().getNIF() exite
+            // TODO test datos
+        try {
+            exito = AyudaJDBC.getInstance().registrarDatosAyuda(ayuda, ayuda.getBeneficiarioDeAyuda().getNIF(), ayuda.getVoluntarioQueOtorga().getNIF());
+        } catch (SQLException ex) {
+            Logger.getLogger(ControladorAyuda.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+        
+        if (ayuda.getTipo_ayuda().isMonetaria()) {
+            exito = ControladorContabilidad.getInstance(null).registrarGastoAyuda(ayuda.getImporte(), ayuda.getObservaciones(), ayuda);
+        }
+        
+        return exito;
+    }
+    
+    private boolean datosNuevoTipoAyuda (String titulo, String description, boolean isMonetaria) {
+        boolean exito = false;
+        // TODO comprobar datos
+        
+        boolean tipoAyudaExiste = true;
+        try {   
+            tipoAyudaExiste = AyudaJDBC.getInstance().comprobarTipoAyuda(titulo);
+        } catch (SQLException ex) {
+            Logger.getLogger(ControladorAyuda.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        if (!tipoAyudaExiste) {
+            TipoAyuda tipoAyuda = new TipoAyuda();
+            tipoAyuda.setDescripcion(description);
+            tipoAyuda.setMonetaria(isMonetaria);
+            tipoAyuda.setTitulo(titulo);
+            
+            try {
+                exito = AyudaJDBC.getInstance().insertarTipoAyuda(tipoAyuda);
+            } catch (SQLException ex) {
+                Logger.getLogger(ControladorAyuda.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        
+        return exito;
+    }
 
+    private boolean eliminarAyuda(Ayuda ayuda) {
+        boolean exito = false;
+
+        try {
+            // TODO jdbc ayuda
+            exito = AyudaJDBC.getInstance().eliminarDatosAyuda(ayuda.getOID());
+        } catch (SQLException ex) {
+            Logger.getLogger(ControladorAyuda.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return exito;
+    }
+
+    private boolean eliminarTipoAyuda(TipoAyuda tipoAyuda) {
+        boolean exito = false;
+        try {
+            exito = AyudaJDBC.getInstance().eliminarDatosTipoAyuda(tipoAyuda.getOID());
+        } catch (SQLException ex) {
+            Logger.getLogger(ControladorAyuda.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return exito;
+    }
     // TODO Listeners de los botones
-
 }
